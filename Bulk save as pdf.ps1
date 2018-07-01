c54406814dafe066c54da08f5a2a2bd3430496bb
Kenneth Teo <kennethteo@msn.com>
2018-07-01 15:57:50 +0800

    Powershell Script for Bulk save as pptx to pdf
    Convert (save-as) all .pptx files in a directory to .pdf

---
+++ Bulk save as pdf.ps1
@@ -0,0 +1,37 @@
+﻿# Add the PowerPoint assemblies that we'll need
+Add-type -AssemblyName office -ErrorAction SilentlyContinue
+Add-Type -AssemblyName microsoft.office.interop.powerpoint -ErrorAction SilentlyContinue
+
+# Start PowerPoint
+$ppt = new-object -com powerpoint.application
+$ppt.visible = [Microsoft.Office.Core.MsoTriState]::msoTrue
+
+# Set the locations where to find the PowerPoint files, and where to store the thumbnails
+$pptPath = "C:\mstemp\Workshop\WsP-NET Fwrk Dev Mod WApps w ASP.NET MVC\PPTs\"
+
+
+# Loop through each PowerPoint File
+Foreach($iFile in $(ls $pptPath -Filter "*.ppt")){
+Set-ItemProperty ($pptPath + $iFile) -name IsReadOnly -value $false
+$filename = Split-Path $iFile -leaf
+$file = $filename.Split(".")[0]
+$oFile = $pptPath + $file + ".pdf" 
+
+# Open the PowerPoint file
+$pres = $ppt.Presentations.Open($pptPath + $iFile)
+
+# Now save it away as PDF 
+$opt= [Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType]::ppSaveAsPDF 
+$pres.SaveAs($ofile,$opt)
+
+
+# and Tidy-up 
+$pres.Close();
+
+}
+
+#Clean Up
+$ppt.quit();
+$ppt = $null
+[gc]::Collect();
+[gc]::WaitForPendingFinalizers();
